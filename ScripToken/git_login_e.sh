898d92d26e3c0ae376d362288e16e07d015bacc5
Miranda00 <diegomiranda0799@gmail.com>
2023-05-26 11:15:35 -0600

    Modifying information

--- ScripToken/git_login_e.sh
+++ ScripToken/git_login_e.sh
@@ -8,8 +8,8 @@
 #git config --global credential.helper 'cache --timeout=3600'
 
 # Leer el nombre de usuario y la contraseña desde el archivo de texto
-read -r USERNAME < user_00.txt
-read -r PASSWORD < git_token_00.txt
+#read -r USERNAME < user_00.txt
+#read -r PASSWORD < git_token_00.txt
 
 # Realizar el git push proporcionando el nombre de usuario y la contraseña
 GIT_ASKPASS=./pass.sh git push
