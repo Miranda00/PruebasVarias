0fb5943a097b1853abfde8b1044d451f32916456
Miranda00 <diegomiranda0799@gmail.com>
2023-05-26 11:12:28 -0600

    Modifying information

--- ScripToken/git_login_e.sh
+++ ScripToken/git_login_e.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Si estás utilizando el protocolo HTTPS para acceder al repositorio remoto, 
+#puedes configurar Git para que recuerde tus credenciales mediante el 
+#comando git config. 
+#Ejecuta los siguientes comandos en tu terminal:
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=3600'
+
 # Leer el nombre de usuario y la contraseña desde el archivo de texto
 read -r USERNAME < user_00.txt
 read -r PASSWORD < git_token_00.txt
--- ScripToken/pass_e.sh
+++ ScripToken/pass_e.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# este archivo pass toma de referencia la password/token de un  archivo txt 
+# echo $(cat password.txt)
+
 echo "$PASSWORD"
